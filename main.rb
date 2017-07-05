d9e4182cb4150e358b47d5d593d8bdf7c29041b1
Meagan Blais <meaganblais@gmail.com>
2017-07-04 20:45:33 -0400

    initial commit

---
+++ main.rb
@@ -0,0 +1,16 @@
+# This is the main entrypoint into the program
+# It requires the other files/gems that it needs
+
+require './players'
+require './game'
+
+## Your test code can go here
+# binding.pry
+
+
+#change below based on requirements
+
+# puts experienced?(@candidates[0])
+# puts find(13)
+# puts qualified_candidates(@candidates)
+# puts ordered_by_qualifications(@candidates)
